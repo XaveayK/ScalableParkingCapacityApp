405671fca7abb6e36d18d754d98e8defe8d567a2
Dakotah M <dakotahspam@gmail.com>
2022-03-30 15:01:07 -0600

    Update serial_monitor.py
    Scaled for all arduinos and sensors and commented

--- serial_monitor.py
+++ serial_monitor.py
@@ -13,6 +13,7 @@ class ardsens: #holds status of object
     self.name = name
     self.active = False
     self.time = 0
+      
 def printCheck(device):
     if device.in_waiting > 0:
         line = device.readline().decode('utf-8').rstrip()
@@ -46,32 +47,104 @@ if __name__ == "__main__":
     try:
         ard1 = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
         ard1.flush()
-
     except:
         print("Ard1 Connection Failed!")
 
+    try:
+        ard2 = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+        ard2.flush()
+    except:
+        print("Ard2 Connection Failed!")
+
+    try:
+        ard3 = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
+        ard3.flush()
+    except:
+        print("Ard3 Connection Failed!")
+
+    try:
+        ard4 = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
+        ard4.flush()
+    except:
+        print("Ard4 Connection Failed!")
+
     while True:
         try:
+            #Grab Serial readout from first device
             HWID1 = printCheck(ard1)
+            #If Hardware ID is fed instead of no response
             if HWID1 != None:
+                #check if sensor is already set to recently active
                 if globals()[HWID1].active != True:
+                    #set sensor to active
                     globals()[HWID1].active = True
+                    #update unix time last active
                     globals()[HWID1].time = int(round(time.time()))
-                    print(globals()[HWID1].active)
+                    #generate API URL based on sensor
                     APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(HWID1)
                     P1 = requests.put(APICall)
                     print(P1)
                     print(P1.content)
                 else:
+                    #if sensor is already active, update time
                     globals()[HWID1].time = int(round(time.time()))
-
         except:
             print("ARD1 Communication Failed!")
+
+        try:
+            HWID2 = printCheck(ard2)
+            if HWID2 != None:
+                if globals()[HWID2].active != True:
+                    globals()[HWID2].active = True
+                    globals()[HWID2].time = int(round(time.time()))
+                    print(globals()[HWID2].active)
+                    APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(HWID2)
+                    P2 = requests.put(APICall)
+                    print(P2)
+                    print(P2.content)
+                else:
+                    globals()[HWID2].time = int(round(time.time()))
+        except:
+            print("ARD2 Communication Failed!")
+
+        try:
+            HWID3 = printCheck(ard3)
+            if HWID3 != None:
+                if globals()[HWID3].active != True:
+                    globals()[HWID3].active = True
+                    globals()[HWID3].time = int(round(time.time()))
+                    print(globals()[HWID3].active)
+                    APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(HWID3)
+                    P3 = requests.put(APICall)
+                    print(P3)
+                    print(P3.content)
+                else:
+                    globals()[HWID3].time = int(round(time.time()))
+        except:
+            print("ARD1 Communication Failed!")
+
+        try:
+            HWID4 = printCheck(ard4)
+            if HWID4 != None:
+                if globals()[HWID4].active != True:
+                    globals()[HWID4].active = True
+                    globals()[HWID4].time = int(round(time.time()))
+                    print(globals()[HWID4].active)
+                    APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(HWID4)
+                    P4 = requests.put(APICall)
+                    print(P4)
+                    print(P4.content)
+                else:
+                    globals()[HWID4].time = int(round(time.time()))
+
+        except:
+            print("ARD4 Communication Failed!")
 
         
 
         for i in sensList:
             currTime = int(round(time.time()))
+            #if sensor is active and it has been over a minute since last trigger
             if i.time <= (currTime-60) and i.active == True:
                 i.active = False
                 print(i.active)
