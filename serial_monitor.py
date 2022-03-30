aa9035013ba523baea71a816f5ebd9503de9cb73
Dakotah M <dakotahspam@gmail.com>
2022-03-30 14:52:41 -0600

    Update serial_monitor.py
    Functionality with API added and tested.

--- serial_monitor.py
+++ serial_monitor.py
@@ -9,34 +9,36 @@ import time
 import serial
 
 class ardsens: #holds status of object
-  def __init__(self):
+  def __init__(self, name):
+    self.name = name
     self.active = False
     self.time = 0
 def printCheck(device):
     if device.in_waiting > 0:
         line = device.readline().decode('utf-8').rstrip()
+        print(line)
         return line
 
 #sensors of arduino 1-4
-A1S1 = ardsens()
-A1S2 = ardsens()
-A1S3 = ardsens()
-A1S4 = ardsens()
+A1S1 = ardsens("A1S1")
+A1S2 = ardsens("A1S2")
+A1S3 = ardsens("A1S3")
+A1S4 = ardsens("A1S4")
 
-A2S1 = ardsens()
-A2S2 = ardsens()
-A2S3 = ardsens()
-A2S4 = ardsens()
+A2S1 = ardsens("A2S1")
+A2S2 = ardsens("A2S2")
+A2S3 = ardsens("A2S3")
+A2S4 = ardsens("A2S4")
 
-A3S1 = ardsens()
-A3S2 = ardsens()
-A3S3 = ardsens()
-A3S4 = ardsens()
+A3S1 = ardsens("A3S1")
+A3S2 = ardsens("A3S2")
+A3S3 = ardsens("A3S3")
+A3S4 = ardsens("A3S4")
 
-A4S1 = ardsens()
-A4S2 = ardsens()
-A4S3 = ardsens()
-A4S4 = ardsens()
+A4S1 = ardsens("A4S1")
+A4S2 = ardsens("A4S2")
+A4S3 = ardsens("A4S3")
+A4S4 = ardsens("A4S4")
 
 sensList = {A1S1, A1S2, A1S3, A1S4, A2S1, A2S2, A2S3, A2S4, A3S1, A3S2, A3S3, A3S3, A4S1, A4S2, A3S3, A4S4}
 
@@ -48,62 +50,32 @@ if __name__ == "__main__":
     except:
         print("Ard1 Connection Failed!")
 
-    try:
-        ard2 = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
-        ard2.flush()
-
-    except:
-        print("Ard2 Connection Failed!")
-
-    try:
-        ard3 = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
-        ard3.flush()
-
-    except:
-        print("Ard3 Connection Failed!")
-
-    try:
-        ard4 = serial.Serial('/dev/ttyACM3', 9600, timeout=1)
-        ard4.flush()
-
-    except:
-        print("Ard4 Connection Failed!")
-
     while True:
         try:
             HWID1 = printCheck(ard1)
-            globals()[HWID1].active = True
-            globals()[HWID1].time = int(round(time.time()))
-            print(globals()[HWID1].active)
-        except:
-            print("ard1 check failed! please check device connection!")
+            if HWID1 != None:
+                if globals()[HWID1].active != True:
+                    globals()[HWID1].active = True
+                    globals()[HWID1].time = int(round(time.time()))
+                    print(globals()[HWID1].active)
+                    APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(HWID1)
+                    P1 = requests.put(APICall)
+                    print(P1)
+                    print(P1.content)
+                else:
+                    globals()[HWID1].time = int(round(time.time()))
 
-        try:
-            HWID2 = printCheck(ard2)
-            globals()[HWID2].active = True
-            globals()[HWID2].time = int(round(time.time()))
-            print(globals()[HWID2].active)
         except:
-            print("ard2 check failed! please check device connection!")
+            print("ARD1 Communication Failed!")
 
-        try:
-            HWID3 = printCheck(ard3)
-            globals()[HWID3].active = True
-            globals()[HWID3].time = int(round(time.time()))
-            print(globals()[HWID3].active)
-        except:
-            print("ard3 check failed! please check device connection!")
-
-        try:
-            HWID4 = printCheck(ard4)
-            globals()[HWID4].active = True
-            globals()[HWID4].time = int(round(time.time()))
-            print(globals()[HWID4].active)
-        except:
-            print("ard4 check failed! please check device connection!")
+        
 
         for i in sensList:
             currTime = int(round(time.time()))
             if i.time <= (currTime-60) and i.active == True:
                 i.active = False
                 print(i.active)
+                APICall = "https://pacific-oasis-59208.herokuapp.com/api/v1/stall-status/"+str(i.name)
+                r = requests.put(APICall)
+                print(r)
+                print(r.content)
